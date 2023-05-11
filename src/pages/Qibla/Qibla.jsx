b665a0b372bc03f156a864505bb5819f076ac531
zakirovravshan <zakirovrovshan@gmail.com>
2023-05-11 08:45:53 +0500

    add

--- src/pages/Qibla/Qibla.jsx
+++ src/pages/Qibla/Qibla.jsx
@@ -24,6 +24,7 @@ export const Qibla = () => {
 						<div className='qibla__frame'>
 							<iframe
 								className='qibla__frame'
+								style={{ marginBottom: '200px' }}
 								src='https://qiblafinder.withgoogle.com/intl/en/onboarding'
 								width='100%'
 								height='100%'
