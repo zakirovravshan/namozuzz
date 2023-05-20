37230fbb987ac3e488abeb3c359f1d9eab8be6b4
zakirovravshan <zakirovrovshan@gmail.com>
2023-05-20 14:37:11 +0500

    add

--- src/pages/Main.js/Main.jsx
+++ src/pages/Main.js/Main.jsx
@@ -204,7 +204,7 @@ const Main = () => {
 										borderRadius: '16px',
 										padding: '5px',
 										textAlign: 'center',
-										marginTop:"5px"
+										marginTop: '5px',
 									}}
 									initial={{ opacity: 0, scale: 0.5 }}
 									animate={{ opacity: 1, scale: 1 }}
